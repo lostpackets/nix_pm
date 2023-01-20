6aa3543ef9fc32f84b893aa91652d407cd205da0
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-20 05:52:54 +0000

    Update auto_install_git_repos_for_nix.sh

--- example_folder_nix/auto_install_git_repos_for_nix.sh
+++ example_folder_nix/auto_install_git_repos_for_nix.sh
@@ -13,6 +13,14 @@ echo "Running the command: nix-prefetch-git https://github.com/$owner/$name.git 
 # Get the git hash and sha256 hash
 nix-prefetch-git https://github.com/$owner/$name.git HEAD
 
+# This reads the keypress instantaneously
+read -p "Do you want to continue? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 output=$(nix-prefetch-git https://github.com/$owner/$name.git HEAD &)
 wait
 echo $output
