8df1c6fb0a94c8c8a336d5becf87211e19010f4f
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-20 14:15:47 +0000

    Update nix_git_repo_installer.sh

--- auto_installers_for_nix_pkg_manager/nix_git_repo_installer.sh
+++ auto_installers_for_nix_pkg_manager/nix_git_repo_installer.sh
@@ -55,3 +55,5 @@ pkgs.stdenv.mkDerivation {
 EOL
 
 echo "Now you can run this github repo with the command: nix-env -i -f $name.nix"
+# It will automatically state the path where it dl'ed, for example: shrinking RPATHs of ELF executables and libraries in /nix/store/hell092fprm2ju3o8qqkhahs4ch-oh-my-bash
+# Run the path /nix/store/hell092fprm2ju3o8qqkhahs4ch-oh-my-bash which should be located at /nix/store/hell092fprm2ju3o8qqkhahs4ch-oh-my-bash/ + "etc/bash.bashrc.d/oh-my-bash/tools/install.sh". My script was written in a way that it copies its dir and sources it at \$src \$out/etc/bash.bashrc.d/$name
