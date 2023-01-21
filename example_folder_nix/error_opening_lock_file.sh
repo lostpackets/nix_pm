e5ce6e0ebd68d34ade6ad84f7824605af5dafece
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-21 04:32:05 +0000

    Create error_opening_lock_file.sh

---
+++ example_folder_nix/error_opening_lock_file.sh
@@ -0,0 +1,5 @@
+#!/usr/bin
+# For errors opening lock file
+# error: opening lock file '/nix/store/kykkvsdh7bd3jlr0p8qzln0hbvg9m0y3-ffmpeg-5.1.2.tar.bz2.drv.lock': Permission denied
+sudo chown -R $USER:staff /nix
+# Then run your nix previous nix command
